932a86c72acf9359b2d156286b59549193684e08
adrianObel <obeladrian@gmail.com>
2012-11-10 18:21:17 -0430

    added error support

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -2,38 +2,40 @@ from sys import argv, exit
 from bs4 import BeautifulSoup
 import urllib2
 
-
-script, lang, word_to_find = argv
-
-if lang == 'spanish' or lang == 'espanol':
-  url = 'http://es.thefreedictionary.com/%s' %  word_to_find
-  search_for_class = 'runseg'
-
-elif lang == 'english' or lang =='ingles':
-  url = 'http://thefreedictionary.com/%s' %  word_to_find
-  search_for_class = 'ds-list'
-
-elif lang == 'italian' or lang == 'italien':
-  url = 'http://it.thefreedictionary.com/%s' % word_to_find
-  search_for_class = 'runseg'
-
+if not len(argv) >= 3:
+  print 'Language or word missing'
 else:
-  print 'Language not supported'
-  exit()
+  script, lang, word_to_find = argv
+  
+  if lang == 'spanish' or lang == 'espanol':
+    url = 'http://es.thefreedictionary.com/%s' %  word_to_find
+    search_for_class = 'runseg'
 
-req = urllib2.urlopen(url)
-data = req.read()
-req.close()
+  elif lang == 'english' or lang =='ingles':
+    url = 'http://thefreedictionary.com/%s' %  word_to_find
+    search_for_class = 'ds-list'
 
-soup = BeautifulSoup(data)
+  elif lang == 'italian' or lang == 'italien':
+    url = 'http://it.thefreedictionary.com/%s' % word_to_find
+    search_for_class = 'runseg'
+
+  else:
+    print 'Language not supported'
+    exit()
+
+  req = urllib2.urlopen(url)
+  data = req.read()
+  req.close()
+
+  soup = BeautifulSoup(data)
 
 
-word_body = soup.find_all('div', class_ = search_for_class)
+  word_body = soup.find_all('div', class_ = search_for_class)
 
-print '\n',word_to_find, '\n\n'
+  print '\n',word_to_find, '\n\n'
 
-if word_body:
-  for text in word_body:
-    print text.get_text()
-else:
-  print "Word not found"
+  if word_body:
+    for text in word_body:
+      print text.get_text()
+  else:
+    print "Word not found"
